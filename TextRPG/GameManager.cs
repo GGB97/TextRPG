2b51bf1048dca3cb16340a1b85a800063c7e0921
GGB <rlqja1018@gmail.com>
2024-01-03 19:47:08 +0900

    Initial commit

---
+++ TextRPG/GameManager.cs
@@ -0,0 +1,10 @@
+﻿namespace TextRPG
+{
+    internal class GameManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TextRPG/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Player
+{
+	int level;
+	string name;
+	string job;
+	int atk;
+	int def;
+	int hp;
+	int gold;
+
+	public Player()
+	{
+		this.level = 1;
+		atk = 10;
+		def = 5;
+		hp = 100;
+		gold = 1500;
+	}
+}
